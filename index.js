67f632963533e8e46d72ce9b16a80ef644043b13
github username <github email address>
2018-06-23 22:19:34 -0400

    implemented functions using filter and map

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, revenue) {
+  return drivers.filter(function(driver) { return driver.revenue > Number.parseInt(revenue); });
+}
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+  return driversWithRevenueOver(drivers, revenue).map(function(driver) { return driver.name; });
+}
+
+function exactMatch(drivers, attributes) {
+  return drivers.filter(function(driver) {
+    for (const key in attributes) {
+        return attributes[key] === driver[key];
+    }
+  });
+}
+
+function exactMatchToList(drivers, attributes) {
+  return exactMatch(drivers, attributes).map(function(driver) { return driver.name; });
+}
+
+// exactMatch()
+// returns an array of all matching drivers ‣
+// ReferenceError: exactMatch is not defined
+//     at Context.<anonymous> (test/indexTest.js:55:7)
+// returns an empty array if there is no match ‣
+// ReferenceError: exactMatch is not defined
+//     at Context.<anonymous> (test/indexTest.js:67:7)
+// exactMatchToList()
+// returns an array of names for all matching drivers ‣
+// ReferenceError: exactMatchToList is not defined
+//     at Context.<anonymous> (test/indexTest.js:75:7)
+// returns an empty array if there is no match ‣
+// ReferenceError: exactMatchToList is not defined
+//     at Context.<anonymous> (test/indexTest.js:81:7)
